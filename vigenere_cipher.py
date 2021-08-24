d80576a35718e138bd6ed2da0dbb157f16d5ea7b
CoreVisional <60378852+CoreVisional@users.noreply.github.com>
2021-08-25 06:41:54 +0800

    Code format

--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -3,14 +3,17 @@ from string import ascii_letters
 
 
 def get_mode(text, key):
-    """
-    Asks the user whether they want to encrypt or decrypt their message.
-    Return statement calls encode_message function or decode_message function depends on the mode that the user chose
-    and translate the message based on the chosen mode. 
-    """
+    """Ask the user whether they want to encrypt or decrypt their message.
 
+    Return statement calls encode_message function or decode_message
+    function depends on the mode that the user chose.
+
+    and translate the message based on the chosen mode.
+
+    """
     while True:
-        mode = input("\n\nDo you wish to encrypt or decrypt the message: ").lower()
+        mode = input("\n\nDo you wish to encrypt or "
+                     "decrypt the message: ").lower()
 
         if mode in "encrypt e".split():
             return encode_message(text, key)
@@ -21,12 +24,14 @@ def get_mode(text, key):
 
 
 def get_message():
-    """
-    Asks the user to enter the message for encryption or decryption.
-    This is repeated until the user enters at least a character into the input prompt.
-    Returns a string containing the user's input.
-    """
+    """Ask the user to enter the message for encryption or decryption.
 
+    This is repeated until the user enters at least a
+    character into the input prompt.
+
+    Returns a string containing the user's input.
+
+    """
     while True:
         message = input("\n\nEnter your message: ")
 
@@ -37,11 +42,14 @@ def get_message():
 
 
 def get_keyword():
-    """
-    Asks the user to enter a keyword that is used to shift the value of each letter in the alphabets
-    and generate a key phrase that has the same length as the message.
+    """Ask the user to enter a keyword that is used to shift the value of
+    each letter in the alphabets and generate a key phrase
+    that has the same length as the message.
+
     This is repeated until the user enters a valid key.
+
     Returns a string containing the user's input.
+
     """
 
     while True:
@@ -54,12 +62,14 @@ def get_keyword():
 
 
 def ask_user_yes_no(yes_no_question):
+    """Simplify if/else in determining the correct answers from the user input.
+
+    Returns True if the user answer the prompt with
+    any of the values in choice_yes.
+
+    Returns False if the user enters any of the values in choice_no.
+
     """
-    To simplify if/else in determining the correct answers from the user input.
-    Returns True if the user answer the prompt with any of the values in choice_yes.
-    Returns False if the user enters any of the values in choice_no
-    """
-    
     choice_yes = ['yes', 'y']
     choice_no = ['no', 'n']
 
@@ -79,27 +89,27 @@ def encode_message(plaintext, key):
     return vigenere_translate(plaintext, key, "encrypt e".split())
 
 
-# Wrapper function to decode message for vigenere_translate function.
 def decode_message(ciphertext, key):
     """Wrapper function to decode message for vigenere_translate function."""
     return vigenere_translate(ciphertext, key, "decrypt d".split())
 
 
 def check_text_case(symbol: str, result: list, key_position: int) -> list:
-    """
-    Checks whether the letter in the message is an uppercase or a lowercase in order to
-    get the translated message to match the casing of the original message.  
+    """Check whether the letter in the message is an uppercase or
+    a lowercase in order to get the translated message
+    to match the casing of the original message.
 
-    Appends the uppercase letter in ascii_letters[key_position] to result 
+    Appends the uppercase letter in ascii_letters[key_position] to result
     if the letter found in symbol is an uppercase letter.
 
-    Appends the lowercase letter in ascii_letters[key_position] to result 
+    Appends the lowercase letter in ascii_letters[key_position] to result
     if the letter found in symbol is an lowercase letter.
 
-    Appends the encrypted (or decrypted) character to result using key_position as the index 
-    to match the position of the letter in ascii_letters
-    """
+    Appends the encrypted (or decrypted) character to result
+    using key_position as the index to match the position
+    of the letter in ascii_letters.
 
+    """
     if symbol.isupper():
         result.append(ascii_letters[key_position].upper())
     elif symbol.islower():
@@ -107,13 +117,15 @@ def check_text_case(symbol: str, result: list, key_position: int) -> list:
 
 
 def check_index_length(index, key):
-    """
-    Checks if the index that is used to match the length of the given keyword is equal to the
-    length of the keyword.
-    Resets index to 0 if index is equal to the length of the keyword and returns an integer of 0
-    Returns index unchanged if index is not equal to the length of the keyword.
-    """
+    """Check if the index that is used to match the length of
+    the given keyword is equal to the length of the keyword.
 
+    Resets index to 0 if index is equal to the length
+    of the keyword and returns an integer of 0.
+
+    Returns index unchanged if index is not equal to the length of the keyword.
+
+    """
     if index == len(key):
         index = 0
         return index
@@ -124,8 +136,8 @@ def print_translated_message(original_message, result, mode):
     """
     Prints the original message entered by the user.
     Prints a string containing the translated message.
-    """
 
+    """
     print(f"\n\nOriginal Message: {original_message}")
 
     if mode == "encrypt e".split():
@@ -135,54 +147,65 @@ def print_translated_message(original_message, result, mode):
 
 
 def vigenere_translate(message, key, mode):
-    """
-    Builds the encrypted (or decrypted) string one character at a time and store 
-    these characters in translated_message.
+    """Build the encrypted (or decrypted) string one character at
+    a time and store  these characters in translated_message.
 
-    The key_index variable keeps track of which subkey to use since a different key is used 
-    depending on the position of the letter in the message.
-    Sets key_index to 0 since the letter used to encrypt or decrypt the first letter of the message will be 
-    the one at key[0] where key variable is the keyword given by the user.
+    The key_index variable keeps track of which subkey to use since a different
+    key is used  depending on the position of the letter in the message.
 
-    The key_index (subkey) will always be what key[key_index] evaluates to.
-    For instance: key_index is 1, keyword is "dog", key[key_index] or dog[1] evaluates to the letter 'o'.
+    Sets key_index to 0 since the letter used to encrypt or decrypt the
+    first letter of the message will be the one at key[0]
+    where key variable is the keyword given by the user.
 
-    Sets the characters in message to the variable char on each iteration of the for loop.
+    The key_index (subkey) will always be what key[key_index] evaluates to;
+    for instance: key_index is 1, keyword is "dog", key[key_index]
+    or dog[1] evaluates to the letter 'o'.
 
-    Finds the index of char in ascii_letters and assigns the result to char_position.
+    Sets the characters in message to the variable char
+    on each iteration of the for loop.
 
-    The letter in char is found in ascii_letters if char_position is not set to -1.
+    Finds the index of char in ascii_letters and
+    assigns the result to char_position.
 
-    Checks if the mode chose by the user is encryption or decryption. 
+    The letter in char is found in ascii_letters
+    if char_position is not set to -1.
+
+    Checks if the mode chose by the user is encryption or decryption.
     += is used for encryption and -= is used for decryption.
-    Mods the integer stored in char_position if the new value of char_position is less than 0 or if the new
-    value of char_position is the length of ascii_letters or greater.
 
-    Increments key_index by 1 after char has been translated in order to use the next subkey on the 
-    next iteration of the for loop.
-    This way when the next iteration uses key[key_index] to get the subkey, it will be the index to the next subkey.
+    Mods the integer stored in char_position if the new value of char_position
+    is less than 0 or if the new value of char_position is
+    the length of ascii_letters or greater.
 
-    Appends the character untranslated if char was not found in ascii_letters. E.g. char is a number 
-    or puncutuation marks such as '10', '?', or ':'.
+    Increments key_index by 1 after char has been translated in order
+    to use the next subkey on the  next iteration of the for loop.
 
-    After building the string from each character and appending it to translated_message, calls print_translated_message
-    and join method on the blank string (the result of appending untranslated character) to join together all 
-    the strings in translated_message.
+    This way, when the next iteration uses key[key_index] to get
+    the subkey, it will be the index to the next subkey.
+
+    Appends the character untranslated if char was not found in
+    ascii_letters e.g. char is a number or puncutuation
+    marks such as '10', '?', or ':'.
+
+    After building the string from each character and appending it to
+    translated_message, calls print_translated_message and join
+    method on the blank string (the result of appending untranslated character)
+    to join together all the strings in translated_message.
+
     """
-
     translated_message = []
-    
+
     key_index = 0
-    
+
     for char in message:
         char_position = ascii_letters.find(char)
-        
+
         if char_position != -1:
             if mode == "encrypt e".split():
                 char_position += ascii_letters.find(key[key_index])
             elif mode == "decrypt d".split():
                 char_position -= ascii_letters.find(key[key_index])
-            
+
             char_position %= len(ascii_letters)
 
             check_text_case(char, translated_message, char_position)
@@ -190,32 +213,35 @@ def vigenere_translate(message, key, mode):
             key_index += 1
 
             key_index = check_index_length(key_index, key)
-        
+
         else:
             translated_message.append(char)
-    
+
     print_translated_message(message, translated_message, mode)
 
 
 def should_translate_again():
-    """
-    Asks the user to enter a message and give a secret keyword in order to encrypt the message. Decrypting uses
-    the same keyword that is used to encrypt the message.
+    """Ask the user to enter a message and give a secret keyword in order to
+    encrypt the message.
 
-    Calls get_mode to check whether the user wants to encrypt or decrypt their message and translates the message 
-    according to the chosen mode.
-    
+    Calls get_mode to check whether the user wants to encrypt or decrypt their
+    messages and translates the message according to the chosen mode.
+
     Asks the user if they want to encrypt or decrypt more messages.
-    Restarts the program if the user wants to perform further encryption or decryption.
-    Exits the program if the user does not want to continue with the program.
-    """
 
+    Restarts the program if the user wants to perform
+    further encryption or decryption.
+
+    Exits the program if the user does not want to continue with the program.
+
+    """
     while True:
         user_message, keyword = get_message(), get_keyword()
 
         get_mode(user_message, keyword)
 
-        if not ask_user_yes_no("\n\nWould you like to encrypt or decrypt another message? (Y/N): "):
+        if not ask_user_yes_no("\n\nWould you like to encrypt or "
+                               "decrypt another message? (Y/N): "):
             print("\n\nExiting program...\n\n")
             break
 
