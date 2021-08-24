a471c03cfd505dc59ef914bb0e1c4edd9985097f
CoreVisional <60378852+CoreVisional@users.noreply.github.com>
2021-08-25 02:46:27 +0800

    Added user input and further code format
    This program now accepts user input to encrypt or decrypt their message.  Further code format has also been performed. A code block containing of a brute-force feature has been added in case needed.

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,54 +1,141 @@
 
-from string import ascii_lowercase as ALPHABETS
+from string import ascii_letters, ascii_lowercase, ascii_uppercase
 
 
-def get_cipher_text(new_shift, character):
+def get_mode():
+    """Return a string containing the user's input.
+
+    Asks the user if they want to encrypt or decrypt their message.
+
+    This is repeated until a valid input is received.
+
     """
-    Returns new character given a new shift key.
-    Returns the original symbol to the decrypted result if a letter in the message is not found in ALPHABETS
+    while True:
+        mode = input("\n\nEncrypt or Decrypt: ").lower()
+        if mode in "encrypt e decrypt d".split():
+            return mode
+        else:
+            print("\nInvalid input. Only 'encrypt', 'e', 'decrypt', or 'd'.")
+
+
+def get_message():
+    """Return a string containing the user's input.
+
+    Calls get_mode to check if the user wants to encrypt or decrypt the message.
+
+    Asks the user to enter the message for encryption or decryption.
+
+    This is repeated until the user enters at least a character into the input prompt.
+
     """
+    get_mode()
 
-    if character in ALPHABETS:
-        return ALPHABETS[new_shift]
-    return character
+    while True:
+        user_message = input("\nType your message: ")
 
+        if user_message != "":
+            return user_message
+        
+        print("\nInput must not be null.")
+        
 
-def decode_ciphered_text(message, offset):
+def get_key():
+    """Return an integer containing the user's input.
+
+    Asks the user to enter a key used for shifting the letters.
+
+    This is repeated until the user enters a valid key.
+
     """
-    Initializes an empty string as the decrypted result from building up each letter.
-    Loops through every letter in the message.
-    Finds the first occurence of the letter and shifts the character with the given POSITIVE offset.
-    Reduces any number greater than 26 to a number from 0 to 25 using the modulo operator.
-    Adds each decrypted letter into decrypted result using new_char_position as an index to the string ALPHABETS
+    key = 0
+
+    while True:
+        try:
+            key = int(input("\nEnter a key number: "))
+        except ValueError:
+            print("\nInput must only be an integer.")
+        else:
+            return key
+
+
+def ask_user_yes_no(yes_no_question):
+    """Simplify if/else in determining the correct answers from the user input.
+
+    Returns True if the user answer the prompt with any of the values in choice_yes.
+
+    Returns False if the user enters any of the values in choice_no.
+
     """
-    
-    decrypted_result = ""
-    
-    for char in message:
-        new_char_position = (ALPHABETS.find(char) + offset) % len(ALPHABETS)
-        decrypted_result += get_cipher_text(new_char_position, char)
+    choice_yes = ['yes', 'y']
+    choice_no = ['no', 'n']
 
-    return decrypted_result
+    while True:
+        user_choice = input(yes_no_question).lower()
+
+        if user_choice in choice_yes:
+            return True
+        elif user_choice in choice_no:
+            return False
+        
+        print("\n\nInvalid Input. Try again.")
 
 
-def encode_plain_text(message, offset):
+def alphabet_position(shift):
+    """Return the new shifted lowercase and uppercase alphabets.
+
+    Shifts the lowercase and uppercase characters given a new key.
+
     """
-    Initializes an empty string as the decrypted result from building up each letter.
-    Loops through every letter in the message.
-    Finds the first occurence of the letter and shifts the character with the given NEGATIVE offset.
-    Reduces any number greater than 26 to a number from 0 to 25 using the modulo operator.
-    Adds each decrypted letter into decrypted result using new_char_position as an index to the string ALPHABETS
+    shifted_lowercase = ascii_lowercase[shift:] + ascii_lowercase[:shift]
+
+    shifted_uppercase = ascii_uppercase[shift:] + ascii_uppercase[:shift]
+
+    return shifted_lowercase, shifted_uppercase
+
+
+def translate_message(message: str, offset):
+    """Return a string of the encrypted message with 
+    each character mapped to the given translation table.
+
+    Rearranges the ordinal numbers of the alphabets 
+    with the given offset including uppercase letters.
+
     """
-    
-    encrypted_message = ""
-    
-    for char in message:
-        new_char_value = (ALPHABETS.find(char) - offset) % len(ALPHABETS)
-        encrypted_message += get_cipher_text(new_char_value, char)
+    lowercase_alpha, uppercase_alpha = alphabet_position(offset)
 
-    return encrypted_message
+    char_index = message.maketrans(ascii_letters, lowercase_alpha + uppercase_alpha)
+
+    print(f"\n\nTranslated Message: {message.translate(char_index)}")
 
 
-print(decode_ciphered_text("jxu evviuj veh jxu iusedt cuiiqwu yi vekhjuud.", 10))
+# def perform_brute_force(message):
+#     """Perform a brute-force attack if the user 
+#     does not know the offset to decrypt the message.
 
-print(encode_plain_text("hey there! this is an example of a caesar cipher. were you able to decode it?", 10))
+#     Prints every possible decryption key from 1 to 26 along with the decrypted message.
+
+#     """
+#     for char in range(1, 26):
+#         print(f"Key: {char}")
+#         print(f"\t{translate_message(message, char)}")
+
+
+def should_translate_again():
+    """Ask whether or not the user wants to encrypt ot decrypt again.
+
+    Restarts the program if the user wants to perform further encryption or decryption.
+
+    Exits the program if the user does not want to continue with the program.
+
+    """
+    while True:
+        user_text, key_rotation = get_message(), get_key()
+        
+        translate_message(user_text, key_rotation)
+
+        if not ask_user_yes_no("\n\nWould you like to encrypt or decrypt another message? (Y/N): "):
+            print("\n\n-----Program Exited-----\n")
+            break
+
+
+should_translate_again()
