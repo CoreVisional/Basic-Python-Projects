9fd5c23e8e776b2cfbfe0589324dc67312d8b65b
CoreVisional <60378852+CoreVisional@users.noreply.github.com>
2021-08-25 06:45:05 +0800

    Code format

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -21,11 +21,13 @@ def get_mode():
 def get_message():
     """Return a string containing the user's input.
 
-    Calls get_mode to check if the user wants to encrypt or decrypt the message.
+    Calls get_mode to check if the user wants
+    to encrypt or decrypt the message.
 
     Asks the user to enter the message for encryption or decryption.
 
-    This is repeated until the user enters at least a character into the input prompt.
+    This is repeated until the user enters at least a
+    character into the input prompt.
 
     """
     get_mode()
@@ -35,9 +37,9 @@ def get_message():
 
         if user_message != "":
             return user_message
-        
+
         print("\nInput must not be null.")
-        
+
 
 def get_key():
     """Return an integer containing the user's input.
@@ -61,7 +63,8 @@ def get_key():
 def ask_user_yes_no(yes_no_question):
     """Simplify if/else in determining the correct answers from the user input.
 
-    Returns True if the user answer the prompt with any of the values in choice_yes.
+    Returns True if the user answer the prompt with
+    any of the values in choice_yes.
 
     Returns False if the user enters any of the values in choice_no.
 
@@ -76,7 +79,7 @@ def ask_user_yes_no(yes_no_question):
             return True
         elif user_choice in choice_no:
             return False
-        
+
         print("\n\nInvalid Input. Try again.")
 
 
@@ -94,25 +97,27 @@ def alphabet_position(shift):
 
 
 def translate_message(message: str, offset):
-    """Return a string of the encrypted message with 
-    each character mapped to the given translation table.
+    """Return a string of the encrypted message with each
+    character mapped to the given translation table.
 
-    Rearranges the ordinal numbers of the alphabets 
-    with the given offset including uppercase letters.
+    Rearranges the ordinal numbers of the alphabets with
+    the given offset including uppercase letters.
 
     """
     lowercase_alpha, uppercase_alpha = alphabet_position(offset)
 
-    char_index = message.maketrans(ascii_letters, lowercase_alpha + uppercase_alpha)
+    char_index = message.maketrans(
+        ascii_letters, lowercase_alpha + uppercase_alpha)
 
     print(f"\n\nTranslated Message: {message.translate(char_index)}")
 
 
 # def perform_brute_force(message):
-#     """Perform a brute-force attack if the user 
-#     does not know the offset to decrypt the message.
+#     """Perform a brute-force attack if the user does
+#     not know the offset to decrypt the message.
 
-#     Prints every possible decryption key from 1 to 26 along with the decrypted message.
+#     Prints every possible decryption key from 1 to 26
+#     along with the decrypted message.
 
 #     """
 #     for char in range(1, 26):
@@ -123,17 +128,19 @@ def translate_message(message: str, offset):
 def should_translate_again():
     """Ask whether or not the user wants to encrypt ot decrypt again.
 
-    Restarts the program if the user wants to perform further encryption or decryption.
+    Restarts the program if the user wants to perform
+    further encryption or decryption.
 
     Exits the program if the user does not want to continue with the program.
 
     """
     while True:
         user_text, key_rotation = get_message(), get_key()
-        
+
         translate_message(user_text, key_rotation)
 
-        if not ask_user_yes_no("\n\nWould you like to encrypt or decrypt another message? (Y/N): "):
+        if not ask_user_yes_no("\n\nWould you like to encrypt or "
+                               "decrypt another message? (Y/N): "):
             print("\n\n-----Program Exited-----\n")
             break
 
