ab8a8ecfcc16d9d27447c232f8349d7ccb1fbf17
Alex Chiew <concussion239@gmail.com>
2021-08-25 02:39:51 +0800

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -2,34 +2,65 @@
 # This program currently can only check for lowercase strings.
 
 
-from string import ascii_lowercase as alphabets
+def encrypt_message(plain_text):
+    """
+    Checks whether or not the string is an uppercase or lowercase from A to Z using the isalpha() string method.
 
+    Adds 26 (there are a total of 26 letters in the English alphabet) to ord_num value if it has a 
+    smaller value than the ordinal value for "a" and then concatenates the encrypted value to enrypted_message.
 
-def decrypt_cipher_text(message, offset):
+    Concatenates the original letter to decrypted_message if the letter was not a lowercase.
+
+    Returns a string of the encrypted message in lowercase.
+    """
+
+    offset = -10
+
+    enrypted_message = ""
+
+    for char in plain_text:
+        if char.isalpha():
+            ord_num = ord(char) + offset
+
+            if ord_num < ord('a'):
+                ord_num += 26 
+
+            enrypted_message += chr(ord_num)
+        else:
+            enrypted_message += char
     
+    return enrypted_message
+
+
+def decrypt_message(plain_text):
+    """
+    Checks whether or not the string is an uppercase or lowercase from A to Z using the isalpha() string method.
+
+    Subtracts 26 (there are a total of 26 letters in the English alphabet) from ord_num value if it has a 
+    larger value than the ordinal value for "z" and then concatenates the decrypted value to decrypted_message.
+
+    Concatenates the original letter to decrypted_message if the letter was not a lowercase.
+
+    Returns a string of the decrypted message in lowercase.
+    """
+
+    offset = 10
+
     decrypted_message = ""
-    
-    for char in message:
-        if char in alphabets:
-            new_char_position = (alphabets.find(char) + offset) % len(alphabets)
-            decrypted_message += alphabets[new_char_position]
+
+    for char in plain_text:
+        if char.isalpha():
+            ord_num = ord(char) + offset
+
+            if ord_num > ord('z'):
+                ord_num -= 26 
+
+            decrypted_message += chr(ord_num)
         else:
             decrypted_message += char
+    
     return decrypted_message
 
+print(encrypt_message("hey there! this is an example of a caesar cipher. were you able to decode it? i hope so! send me a message back with the same offset!"))
 
-def encrypt_plain_text(message, offset):
-    
-    encryted_message = ""
-    
-    for char in message:
-        if char in alphabets:
-            new_char_position = (alphabets.find(char) + offset) % len(alphabets)
-            encryted_message += alphabets[new_char_position]
-        else:
-            encryted_message += char
-    return encryted_message
-
-
-print(decrypt_cipher_text("jxu evviuj veh jxu iusedt cuiiqwu yi vekhjuud.", 10))
-print(encrypt_plain_text("performing multiple caesar ciphers to code your messages is even more secure!", -14))
+print(decrypt_message("xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!"))
