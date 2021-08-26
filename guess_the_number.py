d73efa05c3ed7b7dca9478fae16e4e920606efc0
CoreVisional <60378852+CoreVisional@users.noreply.github.com>
2021-08-26 17:05:50 +0800

    Code reformat

--- guess_the_number.py
+++ guess_the_number.py
@@ -8,7 +8,7 @@ def welcome_user():
 
     This is repeated until the user has provided a username to the program.
     Prints a greeting message along with the player's username.
-    
+
     """
     while True:
         name = input("\n\nEnter your username: ")
@@ -18,7 +18,7 @@ def welcome_user():
             return name
         else:
             print("\nUsername Required!")
-        
+
 
 def validate_input(user_input):
     """Check the user input for a ValueError.
@@ -32,18 +32,24 @@ def validate_input(user_input):
         except ValueError:
             print("\n\nValue must be an integer. \n\nTry again.")
 
-            
+
 def get_upper_bound():
     """Ask the player to set the number that they want to guess up to.
 
-    They may find it difficult to win the game if they set their upper bound to a larger number.
-    If they enter anything that is not an integer value, then a warning message will appear.
+    They may find it difficult to win the game if they set their
+    upper bound to a larger number.
+
+    If they enter anything that is not an integer value,
+    then a warning message will appear.
+
     The game will ask the user to set a number for their upper bound again.
+
     This will be repeated until the user has entered a valid input.
 
     """
     while True:
-        guess_number_maximum = validate_input("\n\nEnter a number for an upper bound: ")
+        guess_number_maximum = validate_input(
+            "\n\nEnter a number for an upper bound: ")
 
         if guess_number_maximum < 1:
             print("\nYour upper bound must be greater than one.")
@@ -54,7 +60,9 @@ def get_upper_bound():
 def ask_user_yes_no(yes_no_question):
     """Simplify if/else in determining the correct answers from the user input.
 
-    Returns True if the user answer the prompt with any of the values in choice_yes.
+    Returns True if the user answer the prompt with
+    any of the values in choice_yes.
+
     Returns False if the user enters any of the values in choice_no.
 
     """
@@ -78,19 +86,26 @@ GUESS_LIMIT = 3
 
 def play_game(upper_bound, player_name):
     """
-    Keep count of the number of times it takes the user to get the correct number.
+    Keep count of the number of times it takes the
+    user to get the correct number.
+
     Increments it by 1 for each attempt by the user.
 
-    Sets the starting range of number to 1 with the ending range set by the user from get_upper_bound function.
-    The guessing range of number is between 1 and up to the number that the user has set.
+    Sets the starting range of number to 1 with the ending range set
+    by the user from get_upper_bound function.
 
-    Checks the user's guessed number and prints out a message to indicate that the guessed number 
-    is either too high or too low than the random number.
+    The guessing range of number is between 1 and up to the
+    number that the user has set.
+
+    Checks the user's guessed number and prints out a message
+    to indicate that the guessed number is either too high
+    or too low than the random number.
 
     Returns True if the user won the game.
     Returns False if the user lost the game.
 
-    Prints out a winning message once the user have guessed the number or out of guesses. 
+    Prints out a winning message once the user have
+    guessed the number or out of guesses.
 
     """
     guess_count = 0
@@ -98,46 +113,61 @@ def play_game(upper_bound, player_name):
     random_number = random.randint(1, upper_bound)
 
     while True:
-        user_number = validate_input("\n\nGuess the number between 1 and " + str(upper_bound) + ": ")
+        user_number = validate_input(
+            "\n\nGuess the number between 1 and " + str(upper_bound) + ": ")
+
         guess_count += 1
-    
+
         if user_number == random_number:
-            print(f"\n\n\U0001F44F Congratulation, {player_name}, you guessed the number!\U0001F44F")
+            print(f"\n\n\U0001F44F Congratulation, {player_name}, "
+                  "you guessed the number!\U0001F44F")
+
             count_guess_attempt(guess_count)
+
             return True
+
         elif user_number > random_number:
             print("Your guessing number is too high. Try guessing lower.")
         else:
             print("Your guessing number is too low. Try guessing higher.")
-        
+
         if guess_count >= GUESS_LIMIT:
-            print(f"\n\nSorry, {player_name}, you lost the game. You ran out of guesses. Better luck next time!")
+            print(f"\n\nSorry, {player_name}, you lost the game. "
+                  "You ran out of guesses. Better luck next time!")
             return False
 
 
 def count_guess_attempt(guess_attempt):
     """Count the number of times it takes the user to guess the number.
 
-    User can only guess the number 3 times. 
-    Prints a message containing the number of times it took the user to guessed the number.
+    User can only guess the number 3 times.
+
+    Prints a message containing the number of times it
+    took the user to guessed the number.
 
     """
     if guess_attempt == 1:
         print(f"\nIt took you {guess_attempt} try to guess the number.\n")
     elif guess_attempt == GUESS_LIMIT:
         print("\nNice One! You guessed the number on your last try.")
-    else:    
+    else:
         print(f"\nIt took you {guess_attempt} tries to guess the number.\n")
 
 
 def should_play_again():
-    """Call greet_user function to greet the user when the program first executes.
+    """Call greet_user function to greet the user
+    when the program first executes.
 
     Starts round of Guess The Number.
 
     Asks the user if they want to play again.
-    Restarts the program if play_game returns True and user wants to play again.
-    Lets the user guess the number again if play_game returns False and user wants a try again.
+
+    Restarts the program if play_game returns True
+    and user wants to play again.
+
+    Lets the user guess the number again if play_game
+    returns False and user wants a try again.
+
     Exits the program if the user does not want to continue with the program.
 
     """
